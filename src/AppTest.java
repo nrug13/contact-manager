211a0c0196213789aa577c2a8c9e36084b4dfe1a
nrug13 <nurgunganbarova@outlook.com>
2023-05-19 19:58:54 +0400

    testing?

---
+++ src/AppTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import impl.ContactIMPL;
+import inter.ContactInterface;
+
+public class AppTest {
+
+}
